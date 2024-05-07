09b2bbdad8a777b958765709154f5f30e00bdf22
kam-ilab <146891307+kam-ilab@users.noreply.github.com>
2024-05-07 13:29:56 +0200

    Added a footer to app.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,30 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Weather defaultCity="Johannesburg" />
-      <footer>
-        This project was coded by Kamila Baas and is
-        <a href="#" target="_blank" rel="noopener noreferrer">
-          open-sourced on GitHub
-        </a>{" "}
-        and <a href="#">hosted on Netlify</a>
-      </footer>
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Weather defaultCity="Johannesburg" />
+        <footer>
+          This project was coded by Kamila Baas and is
+          <a href="#" target="_blank" rel="noopener noreferrer">
+            open-sourced on GitHub
+          </a>{" "}
+          and <a href="#">hosted on Netlify</a>
+        </footer>
+      </div>
     </div>
   );
 }
