69e67d5443b0be4cdf735829d45e322a86eb2d86
kam-ilab <146891307+kam-ilab@users.noreply.github.com>
2024-05-25 16:36:50 +0200

    Improved weather icon and temperature positioning

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,33 +7,27 @@ import "./weather.css";
 export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
-      <h1 className="mt-5">{props.data.city}</h1>
+      <div className="row">
+        <div className="col-6">
+          <h1 className="mt-5">{props.data.city}</h1>
 
-      <span>
-        <FormattedDate date={props.data.date} />
-      </span>
+          <FormattedDate date={props.data.date} />
+        </div>
+        <div className="col-2 weather-app-icon">
+          <WeatherIcon code={props.data.icon} size={64} />
+        </div>
+        <div className="col-4 weather-app-temperature">
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+      </div>
       <br />
-
-      <div className="row mt-1">
+      <div className="row mt-1 mb-5">
         <div className="col-6">
           <span className="text-capitalize">{props.data.description}</span>
           <br />
           <span>Humidity: {props.data.humidity}%</span>
           <br />
           <span>Wind: {props.data.wind} km/h</span>
-        </div>
-      </div>
-
-      <div className="col-6">
-        <div className="weather-app-temperature">
-          <WeatherIcon
-            className="weather-app-icon"
-            code={props.data.icon}
-            size={52}
-          />
-          <div>
-            <WeatherTemperature celsius={props.data.temperature} />
-          </div>
         </div>
       </div>
     </div>
